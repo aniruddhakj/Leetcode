e6750e6743a08c57a8659e2cd66192f4bb45aec4
Aniruddha Krishna Jha <56834666+aniruddhakj@users.noreply.github.com>
2022-04-21 21:16:29 +0530

    Solution to Leetcode 705

---
+++ CPlusPlus/DesignHashset/designHashset.cpp
@@ -0,0 +1,58 @@
+// Author : Aniruddha Krishna Jha
+// Date : 20/04/2022
+
+/*******************************************************************************
+Design a HashSet without using any built-in hash table libraries.
+
+Implement MyHashSet class:
+
+void add(key) Inserts the value key into the HashSet.
+bool contains(key) Returns whether the value key exists in the HashSet or not.
+void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
+ 
+Example 1:
+
+Input
+["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"]
+[[], [1], [2], [1], [3], [2], [2], [2], [2]]
+Output
+[null, null, null, true, false, null, true, null, false]
+
+Explanation
+MyHashSet myHashSet = new MyHashSet();
+myHashSet.add(1);      // set = [1]
+myHashSet.add(2);      // set = [1, 2]
+myHashSet.contains(1); // return True
+myHashSet.contains(3); // return False, (not found)
+myHashSet.add(2);      // set = [1, 2]
+myHashSet.contains(2); // return True
+myHashSet.remove(2);   // set = [1]
+myHashSet.contains(2); // return False, (already removed)
+*******************************************************************************/
+
+class MyHashSet {
+    vector<bool>ans;
+public:
+    MyHashSet() {
+        ans.resize(1e6+1, false);
+    }
+    void add(int key) {
+        ans[key] = true;
+    }
+    
+    void remove(int key) {
+        ans[key] = false;
+    }
+    
+    bool contains(int key) {
+        return ans[key];
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
