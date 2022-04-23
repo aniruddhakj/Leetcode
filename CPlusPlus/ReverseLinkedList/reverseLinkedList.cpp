95f3587fa43b236e696723365e9db12db4b3a4ca
Aniruddha Krishna Jha <56834666+aniruddhakj@users.noreply.github.com>
2022-04-23 21:29:49 +0530

    Solution to Leetcode 206

---
+++ CPlusPlus/ReverseLinkedList/reverseLinkedList.cpp
@@ -0,0 +1,43 @@
+// Author : Aniruddha Krishna Jha
+// Date : 23/04/2022
+
+/*******************************************************************************
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Example 1:
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Example 2:
+
+Input: head = [1,2]
+Output: [2,1]
+
+Example 3:
+
+Input: head = []
+Output: []
+*******************************************************************************/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head || !(head->next))
+            return head;
+        ListNode* last = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return last;
+    }
+};
