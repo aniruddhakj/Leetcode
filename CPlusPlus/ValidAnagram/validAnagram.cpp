bedb9b5db3891a398ec9093667cc98349b948483
Aniruddha Krishna Jha <56834666+aniruddhakj@users.noreply.github.com>
2022-08-09 18:21:32 +0530

    Update validAnagram.cpp
    Updated solution with simpler approach and more readable code

--- CPlusPlus/ValidAnagram/validAnagram.cpp
+++ CPlusPlus/ValidAnagram/validAnagram.cpp
@@ -18,25 +18,22 @@ Example 2:
 Input: s = "rat", t = "car"
 Output: false
 *******************************************************************************/
-
 class Solution {
-    vector<int> createVector(string s){
-        vector<int> mapS(26);
-        for(char c: s) mapS[c-'a']++;
-        return mapS;
-    }
-    bool areVectorsEqual(vector<int> a, vector<int> b){
-        for(int i=0; i<26; i++){
-            if(a[i]!=b[i]) return false;
-        }
-        return true;
-    }
 public:
     bool isAnagram(string s, string t) {
-        if(s.size() != t.size()) return false;
-        vector<int> mapS(26), mapT(26);
-        mapS = createVector(s);
-        mapT = createVector(t);
-        return areVectorsEqual(mapS, mapT);
+        if(s.size() != t.size())
+            return false;
+        
+        vector<int>cnt(26, 0); //map to store cnt of each char
+        
+        for(char ch : s){
+            cnt[ch - 'a']++; //increment cnt of ch foreach appearance in s
+        }
+        for(char ch : t){
+            cnt[ch - 'a']--; //decrement cnt for each appearance in t
+            if(cnt[ch - 'a'] < 0) // if cnt ch in s ≠ ch in t
+                return false;
+        }
+        return true;
     }
 };
