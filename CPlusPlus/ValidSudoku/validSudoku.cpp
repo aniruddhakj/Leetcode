5a2f6a0776bf68e15c32b79ff69b7b7d7bb125c3
Aniruddha Krishna Jha <56834666+aniruddhakj@users.noreply.github.com>
2022-08-10 09:53:08 +0530

    Update validSudoku.cpp

--- CPlusPlus/ValidSudoku/validSudoku.cpp
+++ CPlusPlus/ValidSudoku/validSudoku.cpp
@@ -1,5 +1,5 @@
 // Author : Aniruddha Krishna Jha   
-// Date   : 21/08/2021
+// Date   : 10/08/2022
 
 /********************************************************************************** 
 Determine if a 9 x 9 Sudoku board is valid.
@@ -17,38 +17,30 @@ Note:
 
 class Solution {
 public:
-    bool isValidSudoku(vector<vector<char> > &board) {
+    bool isValidSudoku(vector<vector<char>>& board) {
         const int cnt = 9;
-        bool row_mask[cnt][cnt] = {false};
-        bool col_mask[cnt][cnt] = {false};
-        bool area_mask[cnt][cnt] = {false};
-        //check each rows and cols
-        for(int r=0; r<board.size(); r++){
-            for (int c=0; c<board[r].size(); c++){
-                if (!isdigit(board[r][c])) continue;
-                int index =  board[r][c] - '0' - 1;
+        bool row[cnt][cnt] = {false};
+        bool col[cnt][cnt] = {false};
+        bool sub[cnt][cnt] = {false};
+        
+        for(int r = 0; r < cnt; ++r){
+            for(int c = 0; c < cnt; ++c){
+                if(board[r][c] == '.')
+                    continue; // if not number pass
                 
-                //check the rows
-                if (row_mask[r][index] == true){
-                    return false;
-                }
-                row_mask[r][index] = true;
-                
-                //check the cols
-                if (col_mask[c][index] == true) {
-                    return false;
-                }
-                col_mask[c][index] = true;
-                
-                //check the areas
+                int idx = board[r][c] - '0' - 1; //char to num idx
                 int area = (r/3) * 3 + (c/3);
-                if (area_mask[area][index] == true) {
+                
+                //if number already exists
+                if(row[r][idx] || col[c][idx] || sub[area][idx]){
                     return false;
                 }
-                area_mask[area][index] = true;
+                
+                row[r][idx] = true;
+                col[c][idx] = true;
+                sub[area][idx] = true;
             }
         }
-        
         return true;
     }
 };
