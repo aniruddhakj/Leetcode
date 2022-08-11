605bd0d8749322910130b829ef25dd8d04cd8849
Aniruddha Krishna Jha <56834666+aniruddhakj@users.noreply.github.com>
2022-08-11 13:39:13 +0530

    Solution to Leetcode 128

---
+++ CPlusPlus/LongestConsecutiveSequence/longestConsecutiveSequence.cpp
@@ -0,0 +1,38 @@
+// Author : Aniruddha Krishna Jha
+// Date : 11/08/2022
+
+/*******************************************************************************
+Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+You must write an algorithm that runs in O(n) time.
+
+Example 1:
+
+Input: nums = [100,4,200,1,3,2]
+Output: 4
+Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+
+Example 2:
+
+Input: nums = [0,3,7,2,5,8,4,6,0,1]
+Output: 9
+*******************************************************************************/
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int>s(nums.begin(), nums.end());
+        int longest = 0;
+        for(auto &n: s){
+            //if this is the start of the sequence
+            if(!s.count(n - 1)){
+                int length = 1; 
+                while(s.count(n + length))
+                    ++length;
+                longest = max(longest, length);
+            } 
+
+        }
+        return longest;
+    }
+};
