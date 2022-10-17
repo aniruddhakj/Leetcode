da3d70afd7fca3cd340e600bd1d1fab8ba790a23
Aniruddha Krishna Jha <56834666+aniruddhakj@users.noreply.github.com>
2022-10-17 21:50:48 +0530

    Update Leetcode 424

--- C++/LongestRepeatingCharacterReplacement/longestRepeatingCharacterReplacement.cpp
+++ C++/LongestRepeatingCharacterReplacement/longestRepeatingCharacterReplacement.cpp
@@ -1,39 +1,46 @@
 // Author : Aniruddha Krishna Jha
-// Date : 16/08/2022
+// Date : 17/10/2022
 
 /*******************************************************************************
-You are given a string s and an integer k. 
+You are given a string s and an integer k.
 You can choose any character of the string and change it to any other uppercase English character. 
 You can perform this operation at most k times.
 
 Return the length of the longest substring containing the same letter you can get after performing the above operations.
-
 
 Example 1:
 
 Input: s = "ABAB", k = 2
 Output: 4
 Explanation: Replace the two 'A's with two 'B's or vice versa.
+
+Example 2:
+
+Input: s = "AABABBA", k = 1
+Output: 4
+Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+The substring "BBBB" has the longest repeating letters, which is 4.
 *******************************************************************************/
+
+// INTUITION: window = end - start + 1, mc is max appearing char in a window
+// if there are more char in a window then replacements possible
+// move the window (start++), else update the length
 
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        vector<int> cnt(26, 0); //window map
-        int l = 0, r = 0, mlen = 0, mc = 0 ;
-    
-        while(r < s.size()){
-            cnt[s[r] - 'A']++;
-            mc = max(mc, cnt[s[r] - 'A']); //store max freq. char
-            if(r - l + 1 - mc > k){ //if maxfreq char remains in window after replacement
-                cnt[s[l] - 'A']--; //move the window's left end
-                l++;
+        vector<int> cnt(26, 0);
+        int start = 0, end = 0, mc = 0, mlen = 0;
+        while(end < s.size()){
+            cnt[s[end] - 'A']++;
+            mc = max(mc, cnt[s[end] - 'A']);
+            if(end - start + 1 - mc > k){
+                cnt[s[start] - 'A']--;
+                start++;
             }
-            mlen = max(mlen, r - l + 1); 
-            r++;
-               
-            
-        }        
+            mlen = max(mlen, end - start + 1);
+            end++;
+        }
         return mlen;
     }
 };
