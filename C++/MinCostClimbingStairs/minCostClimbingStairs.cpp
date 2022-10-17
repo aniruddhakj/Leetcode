0994da36728f209678ad6cf4f72896d6c286af1d
Aniruddha Krishna Jha <56834666+aniruddhakj@users.noreply.github.com>
2022-10-17 16:02:42 +0530

    Solution to Leetcode 746

---
+++ C++/MinCostClimbingStairs/minCostClimbingStairs.cpp
@@ -0,0 +1,49 @@
+// Author : Aniruddha Krishna Jha
+// Date : 16/10/2022
+
+/*******************************************************************************
+You are given an integer array cost where cost[i] is the cost of ith step on a staircase. 
+Once you pay the cost, you can either climb one or two steps.
+
+You can either start from the step with index 0, or the step with index 1.
+
+Return the minimum cost to reach the top of the floor.
+
+Example 1:
+
+Input: cost = [10,15,20]
+Output: 15
+Explanation: You will start at index 1.
+- Pay 15 and climb two steps to reach the top.
+The total cost is 15.
+
+Example 2:
+
+Input: cost = [1,100,1,1,1,100,1,1,100,1]
+Output: 6
+Explanation: You will start at index 0.
+- Pay 1 and climb two steps to reach index 2.
+- Pay 1 and climb two steps to reach index 4.
+- Pay 1 and climb two steps to reach index 6.
+- Pay 1 and climb one step to reach index 7.
+- Pay 1 and climb two steps to reach index 9.
+- Pay 1 and climb one step to reach the top.
+The total cost is 6.
+*******************************************************************************/
+
+//INTUITION: dp[i] = cost[i] + min(dp[i + 1], dp[i + 2]). 
+// Res will be the min(dp[0], dp[1]) 
+
+// inplace, for every step i, 
+// take min(i + 1, i + 2) to get least cost
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        
+        for(int i = cost.size() - 3; i >= 0; --i)
+            cost[i] += min(cost[i + 1], cost[i + 2]);
+        
+        return min(cost[0], cost[1]);
+    }
+};
