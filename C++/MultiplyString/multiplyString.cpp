7b0ce7f4250c922cad41bf51a32ab2c2d8d085b7
Aniruddha Krishna Jha <56834666+aniruddhakj@users.noreply.github.com>
2022-10-23 19:34:46 +0530

    Update multiplyString.cpp

--- C++/MultiplyString/multiplyString.cpp
+++ C++/MultiplyString/multiplyString.cpp
@@ -2,49 +2,47 @@
 // Date : 23/10/2022
 
 /*******************************************************************************
-Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
+
+Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.
+
 Example 1:
-Input: head = [1,2,2,1]
-Output: true
+
+Input: num1 = "2", num2 = "3"
+Output: "6"
+
 Example 2:
-Input: head = [1,2]
-Output: false
+
+Input: num1 = "123", num2 = "456"
+Output: "56088"
 *******************************************************************************/
 
-// INTUITION: find middle using fast and slow pointers
-// reverse the second half and check for equality
+// INTUITION: Idea is to visualise and apply the 
+// normal vertical multiplication method
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        ListNode *slow = head, *fast = head, *rev = nullptr;
-        // find the middle
-        while(fast && fast->next) {
-            ListNode* tmp = rev;
-            fast = fast->next->next;
-            rev = slow; 
-            slow = slow->next;
-            rev->next = tmp;
-
+    string multiply(string num1, string num2) {
+        int m = num1.size(), n = num2.size();
+        vector<int> res(m + n, 0);
+        for(int i = m - 1; i >= 0; --i) {
+            for(int j = n - 1; j >= 0; --j) {
+                // multiplication result of x * y =  a   b stored
+                //                                   |   |
+                // at                               i+j i+j+1
+                // tens unit wil 
+                res[i + j + 1] += (num1[i]-'0') * (num2[j]-'0');
+                res[i + j] += res[i + j + 1] / 10;
+                res[i + j + 1] %= 10;
+                
+            }
         }
-        if(fast) slow = slow->next;
-
-        while(rev) {
-            if(rev->val != slow->val) return false;
-            slow = slow->next;
-            rev = rev->next;
+        
+        string ans = "";
+        for (int pos: res) {
+            if(!(ans.size() == 0 && pos == 0)) ans += to_string(pos);
         }
-
-        return true;
+        
+        return !ans.size() ? "0" : ans;
     }
 };
