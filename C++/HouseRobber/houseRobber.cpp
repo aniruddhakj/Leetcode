daa4caec4b5aca4bdd55dc1e93e7433fde2b82e8
Aniruddha Krishna Jha <56834666+aniruddhakj@users.noreply.github.com>
2022-10-23 20:14:19 +0530

    Update houseRobber.cpp

--- C++/HouseRobber/houseRobber.cpp
+++ C++/HouseRobber/houseRobber.cpp
@@ -17,18 +17,17 @@ Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
 Total amount you can rob = 1 + 3 = 4.
 *******************************************************************************/
 
+// INTUITION: 2 variables for the two paths 
+
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        if(nums.size() == 0) return 0;
-        if(nums.size() == 1) return nums[0];
-        if(nums.size() == 2) return max(nums[0], nums[1]);
-        vector<int> dp(nums.size(), 0);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);  //whichever house has more money, we can rob it
-        for(int i = 2; i < nums.size(); i++){
-            dp[i] = max(dp[i-2] + nums[i], dp[i-1]);   //if we rob the current house, we can't rob the previous house
+        int pre = 0, cur = 0;
+        for(int n : nums) {
+            int temp = max(pre + n, cur);
+            pre = cur;
+            cur = temp; //update rob2
         }
-        return dp[nums.size()-1];
+        return cur;
     }
 };
